46d629f0107e6f24b8b700d0b2158c5433ae0695
Jessica Campbell <jesslyn.campbell@gmail.com>
2015-02-04 13:16:20 -0600

    Create new cities

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
+var urlencode = bodyParser.urlencoded({ extended: false });
 app.use(express.static('public'));
 
+var cities = {
+  'Lotopia': 'Lotopia description',
+  'Caspiana': 'Caspiana description',
+  'Indigo': 'Indigo description'
+};
+
 app.get('/cities', function(req, res) {
-  var cities = ['Lotopia', 'Caspiana', 'Indigo'];
-  res.json(cities);
+  var cityNames = Object.keys(cities);
+  res.json(cityNames);
+});
+
+app.post('/cities', urlencode, function(req, res) {
+  var newCity = req.body;
+  cities[newCity.name] = newCity.description;
+  res.status(201).json(newCity.name);
 });
 
 module.exports = app;
--- test.js
+++ test.js
@@ -1,9 +1,9 @@
 var request = require('supertest');
 var app = require('./app');
 
-describe('Requests to the root path', function () {
+describe('Requests to the root path', function() {
 
-  it('Returns a 200 status code', function (done) {
+  it('Returns a 200 status code', function(done) {
     request(app)
       .get('/')
       .expect(200, done);
@@ -23,7 +23,7 @@ describe('Requests to the root path', function () {
 
 });
 
-describe('Listing cities on /cities', function () {
+describe('Listing cities on /cities', function() {
 
   it('Returns 200 status code', function(done) {
     request(app)
@@ -37,10 +37,28 @@ describe('Listing cities on /cities', function () {
       .expect('Content-Type', /json/, done);
   });
 
-  it('Returns initial cities', function (done) {
+  it('Returns initial cities', function(done) {
     request(app)
       .get('/cities')
       .expect(JSON.stringify(['Lotopia', 'Caspiana', 'Indigo']), done);
+  });
+
+});
+
+describe('Creating new cities', function() {
+
+  it('Returns a 201 status code', function(done) {
+    request(app)
+      .post('/cities')
+      .send('name=Springfield&description=where+the+simpsons+live')
+      .expect(201, done);
+  });
+
+  it('Return the city name', function(done) {
+    request(app)
+      .post('/cities')
+      .send('name=Springfield&description=where+the+simpsons+live')
+      .expect(/springfield/i, done);
   });
 
 });
